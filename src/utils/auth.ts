09b154d7487d09e81c449622ce13b87613b28d49
DevFarhanCoder <154915648+DevFarhanCoder@users.noreply.github.com>
2025-08-06 16:32:37 +0530

    Update auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,11 @@
-export const requireLogin = () => {
+export const requireLogin = (): false | { token: string } => {
   const user = JSON.parse(localStorage.getItem("user") || "null");
+
   if (!user) {
     alert("Please create an account to make a payment.");
     window.location.href = "/signup";
     return false;
   }
+
   return user;
 };
