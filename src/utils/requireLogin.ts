d850aef74b9339e294ccae8925a779a8d0c3600c
DevFarhanCoder <154915648+DevFarhanCoder@users.noreply.github.com>
2025-08-07 09:24:48 +0530

    Update requireLogin.ts

--- src/utils/requireLogin.ts
+++ src/utils/requireLogin.ts
@@ -1,5 +1,6 @@
 export function requireLogin() {
   const user = JSON.parse(localStorage.getItem('user') || 'null');
+  console.log("user in requireLogin:", user);
   if (!user) {
     alert("Please create an account to make a payment.");
     return null;
